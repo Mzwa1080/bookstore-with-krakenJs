d03b10a48fd4335995896e61de5b0fae1d5c5d8f
Mzwa <mshawnlouw@gmail.com>
2019-01-30 10:41:38 +0200

    Day 2 - new Section added!

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var server;
 
 server = http.createServer(app);
 
-server.listen(process.env.PORT || 8020);
+server.listen(process.env.PORT || 8044);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
 });
